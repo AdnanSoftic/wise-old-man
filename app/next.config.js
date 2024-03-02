8e46041f0ee70f29415424e07a0d6fafdafef86e
Adnan Softic <adnan.softic@gmail.com>
2024-03-02 08:32:46 +0100

    Update next.config.js to output as standalone

--- app/next.config.js
+++ app/next.config.js
@@ -1,103 +1,111 @@
 /** @type {import('next').NextConfig} */
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-const withSvgr = require("next-plugin-svgr");
-
-const nextConfig = withBundleAnalyzer(
-  withSvgr({
-    logging: {
-      fetches: {
-        fullUrl: true,
-      },
-    },
-    experimental: {
-      windowHistorySupport: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "wiseoldman.ams3.cdn.digitaloceanspaces.com",
-        },
-      ],
-    },
-    async redirects() {
-      const externalRedirects = [
-        // Redirects to external websites
-        {
-          source: "/github",
-          destination: "https://github.com/wise-old-man/wise-old-man",
-          permanent: true,
-        },
-        {
-          source: "/twitter",
-          destination: "https://twitter.com/RubenPsikoi",
-          permanent: true,
-        },
-        {
-          source: "/discord",
-          destination: "https://discordapp.com/invite/Ky5vNt2",
-          permanent: true,
-        },
-        {
-          source: "/patreon",
-          destination: "https://patreon.com/wiseoldman",
-          permanent: true,
-        },
-        {
-          source: "/flags",
-          destination: `https://github.com/wise-old-man/wise-old-man/wiki/User-Guide:-How-to-setup-countries-flags`,
-          permanent: true,
-        },
-        {
-          source: "/docs",
-          destination: "https://docs.wiseoldman.net",
-          permanent: true,
-        },
-      ];
-
-      if (process.env.MAINTENANCE_MODE_ENABLED) {
-        return [
-          ...externalRedirects,
-          {
-            source: "/((?!maintenance).*)",
-            destination: "/maintenance",
-            permanent: false,
-          },
-        ];
-      }
-
-      return [
-        ...externalRedirects,
-        {
-          source: "/leaderboards",
-          destination: "/leaderboards/top",
-          permanent: true,
-        },
-        {
-          source: "/ehp",
-          destination: "/ehp/main",
-          permanent: true,
-        },
-        {
-          source: "/ehb",
-          destination: "/ehb/main",
-          permanent: true,
-        },
-        // RuneLite link redirects (old app's url format -> new app's url format)
-        {
-          source: "/players/:username/gained/skilling",
-          destination: "/players/:username/gained",
-          permanent: true,
-        },
-      ];
-    },
-  }),
-);
-
-nextConfig.output = "standalone";
+const nextConfig = {
+  output: 'standalone'
+};
 
 module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// const withSvgr = require("next-plugin-svgr");
+
+// const nextConfig = withBundleAnalyzer(
+//   withSvgr({
+//     logging: {
+//       fetches: {
+//         fullUrl: true,
+//       },
+//     },
+//     experimental: {
+//       windowHistorySupport: true,
+//     },
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: "https",
+//           hostname: "wiseoldman.ams3.cdn.digitaloceanspaces.com",
+//         },
+//       ],
+//     },
+//     async redirects() {
+//       const externalRedirects = [
+//         // Redirects to external websites
+//         {
+//           source: "/github",
+//           destination: "https://github.com/wise-old-man/wise-old-man",
+//           permanent: true,
+//         },
+//         {
+//           source: "/twitter",
+//           destination: "https://twitter.com/RubenPsikoi",
+//           permanent: true,
+//         },
+//         {
+//           source: "/discord",
+//           destination: "https://discordapp.com/invite/Ky5vNt2",
+//           permanent: true,
+//         },
+//         {
+//           source: "/patreon",
+//           destination: "https://patreon.com/wiseoldman",
+//           permanent: true,
+//         },
+//         {
+//           source: "/flags",
+//           destination: `https://github.com/wise-old-man/wise-old-man/wiki/User-Guide:-How-to-setup-countries-flags`,
+//           permanent: true,
+//         },
+//         {
+//           source: "/docs",
+//           destination: "https://docs.wiseoldman.net",
+//           permanent: true,
+//         },
+//       ];
+
+//       if (process.env.MAINTENANCE_MODE_ENABLED) {
+//         return [
+//           ...externalRedirects,
+//           {
+//             source: "/((?!maintenance).*)",
+//             destination: "/maintenance",
+//             permanent: false,
+//           },
+//         ];
+//       }
+
+//       return [
+//         ...externalRedirects,
+//         {
+//           source: "/leaderboards",
+//           destination: "/leaderboards/top",
+//           permanent: true,
+//         },
+//         {
+//           source: "/ehp",
+//           destination: "/ehp/main",
+//           permanent: true,
+//         },
+//         {
+//           source: "/ehb",
+//           destination: "/ehb/main",
+//           permanent: true,
+//         },
+//         // RuneLite link redirects (old app's url format -> new app's url format)
+//         {
+//           source: "/players/:username/gained/skilling",
+//           destination: "/players/:username/gained",
+//           permanent: true,
+//         },
+//       ];
+//     },
+//   }),
+// );
+
+// nextConfig.output = "standalone";
+// module.exports = nextConfig;
